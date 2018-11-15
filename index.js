5a4d6f4e209879577c4cfa57d3b29b545cb26294
luiscmartinez <mrluismartinezzz@gmail.com>
2018-11-14 19:05:48 -0800

    fixed file paths

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('express')()
 require('./middleware')(server)
-require('./api/routes')(server)
+require('./routes')(server)
 
 server.listen(9000, () => console.log('\n=== Port is open ===\n'))
---
+++ routes/cohortRouter.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const { cohorts } = require('../controllers')
+
+router.post('/', cohorts.addCohort)
+
+router.get('/:id/students', cohorts.getAllCohortStudents)
+
+router.get('/', cohorts.getAllCohorts)
+
+router.get('/:id', cohorts.getSingleCohort)
+
+router.put('/:id', cohorts.updateCohort)
+
+router.delete('/:id', cohorts.deleteCohort)
+
+module.exports = router
